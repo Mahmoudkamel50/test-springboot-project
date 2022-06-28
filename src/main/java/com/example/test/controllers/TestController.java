8b23fc54a6f56441eb3e7747183ab76a1682f0a8
Mahmoud <mk687904@gmail.com>
2022-06-28 18:40:59 +0200

    Added m1 api

---
+++ src/main/java/com/example/test/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.example.test.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/m1")
+    public String m1(){
+        return "m1";
+    }
+}
