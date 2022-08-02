86fb9983dfba2517f9ca5d80bb9dba3b43a16299
Mahmoud <mk687904@gmail.com>
2022-08-02 23:31:51 +0200

    Rest template in spring boot

--- src/main/java/com/example/test/controllers/TestController.java
+++ src/main/java/com/example/test/controllers/TestController.java
@@ -1,15 +1,34 @@
 package com.example.test.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URL;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/myapp")
 public class TestController {
 
-    @GetMapping("/m2")
-    public String m2(){
-        return "m2";
+    @Autowired
+    private RestTemplate restTemplate;
+
+    String url= "https://endpoint.free.beeceptor.com/my/api/path/favicon.ico";
+    @GetMapping
+    public Object getApi(){
+       String response= restTemplate.getForObject(url, String.class);
+       return response;
     }
+
+    /**
+     * here i tried to solve it by passing the url throw the method but i faced an error
+    @GetMapping(path = "/test")
+    public ResponseEntity<?> getApi2(URL url){
+       return new  ResponseEntity<String>(restTemplate.getForEntity(url, String.class), HttpStatus.OK);
+    }
+    */
 }
