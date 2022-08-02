a8ae2d12b662e1cbcaa76513bdf08d0d28817129
Mahmoud <mk687904@gmail.com>
2022-08-02 23:33:00 +0200

    removed none important imports

--- src/main/java/com/example/test/controllers/TestController.java
+++ src/main/java/com/example/test/controllers/TestController.java
@@ -1,14 +1,10 @@
 package com.example.test.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.net.URL;
 
 @RestController
 @RequestMapping("/myapp")
