ce425d88d913f012eb4ce4f69bb56eaf9a0a0b85
Mahmoud <mk687904@gmail.com>
2022-08-07 13:05:54 +0200

    resolved the method rest template

--- src/main/java/com/example/test/controllers/TestController.java
+++ src/main/java/com/example/test/controllers/TestController.java
@@ -3,6 +3,7 @@ package com.example.test.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,18 +14,10 @@ public class TestController {
     @Autowired
     private RestTemplate restTemplate;
 
-    String url= "https://endpoint.free.beeceptor.com/my/api/path/favicon.ico";
-    @GetMapping
-    public Object getApi(){
-       String response= restTemplate.getForObject(url, String.class);
-       return response;
+    @GetMapping(path = "/test")
+    public Object getApi(@RequestParam String api){
+        String response= restTemplate.getForObject(api, String.class);
+        return response;
     }
 
-    /**
-     * here i tried to solve it by passing the url throw the method but i faced an error
-    @GetMapping(path = "/test")
-    public ResponseEntity<?> getApi2(URL url){
-       return new  ResponseEntity<String>(restTemplate.getForEntity(url, String.class), HttpStatus.OK);
-    }
-    */
 }
