adee836ba9da95c7ac004b4dabc3e88121b6fb81
Mahmoud <mk687904@gmail.com>
2022-10-19 20:51:48 +0200

    expectedStatus taken by user

--- src/main/java/com/example/test/controllers/RedirectController.java
+++ src/main/java/com/example/test/controllers/RedirectController.java
@@ -14,26 +14,8 @@ public class RedirectController {
     private RedirectService redirectService;
 
     @PostMapping
-    public String postApi(@RequestParam String api, @RequestParam String methodType, @RequestBody String body) {
-        log.info("Start request with data: api={}, methodType={}, body={}", api, methodType, body);
-        return redirectService.sendRequest(api, methodType, body, 200);
-    }
-
-    @GetMapping
-    public String getApi(@RequestParam String api, @RequestParam String methodType) {
-        log.info("Start request with data: api={}, methodType={}", api, methodType);
-        return redirectService.sendRequest(api, methodType, null, 200);
-    }
-
-    @PutMapping
-    public String updateApi(@RequestParam String api, @RequestParam String methodType, @RequestBody String body) {
-        log.info("Start request with data: api={}, methodType={}, body={}", api, methodType,body);
-        return redirectService.sendRequest(api, methodType, body, 200);
-    }
-
-    @DeleteMapping
-    public String deleteApi(@RequestParam String api, @RequestParam String methodType) {
-        log.info("Start request with data: api={}, methodType={}", api, methodType);
-        return redirectService.sendRequest(api, methodType, null, 200);
+    public String postApi(@RequestParam String api, @RequestParam String methodType, @RequestBody String body, @RequestParam int expectedStatus) {
+        log.info("Start request with data: api={}, methodType={}, body={}, expectedStatus={}", api, methodType, body, expectedStatus);
+        return redirectService.sendRequest(api, methodType, body, expectedStatus);
     }
 }
